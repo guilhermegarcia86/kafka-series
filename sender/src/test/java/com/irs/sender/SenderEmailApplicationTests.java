0cb08ca76035c50a21732d25a8277b0ead2d29d8
Guilherme Alves <guilherme.jose@wirecard.com>
2021-01-05 12:10:52 -0300

    Iniciado consumer

---
+++ sender/src/test/java/com/irs/sender/SenderEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irs.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

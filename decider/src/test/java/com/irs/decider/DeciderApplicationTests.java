7ae139bff98d6c4eafae231c3ae412a791f746f4
Guilherme Garcia Alves <guilherme.garcia86@gmail.com>
2021-02-10 15:15:17 -0300

    Iniciado testes Kafka Streams

--- decider/src/test/java/com/irs/decider/DeciderApplicationTests.java
+++ decider/src/test/java/com/irs/decider/DeciderApplicationTests.java
@@ -1,12 +1,7 @@
 package com.irs.decider;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class DeciderApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
